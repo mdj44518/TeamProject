ce25473c6f98fe70a803cb9417e3368342854a33
YongHo <YongHo@DESKTOP-4O562D1>
2019-12-20 17:29:18 +0900

    jsp page 확인을 위한 코드수정
    return값 hello -> contents로

--- TeamProject/src/main/java/controller/HelloController.java
+++ TeamProject/src/main/java/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 	public String hello(Model model,
 			@RequestParam(value = "name", required = false) String name) {
 		model.addAttribute("greeting", "안녕하세요, " + name);
-		return "hello";
+		return "contents";
 	}
 }
