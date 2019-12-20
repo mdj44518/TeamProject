363acdf929906e8d012e0df1fcc5acb032926f7e
sonle <sonle@DESKTOP-SIOSBEI>
2019-12-20 18:58:02 +0900

    이순근1

--- TeamProject/src/main/java/controller/HelloController.java
+++ TeamProject/src/main/java/controller/HelloController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import dao.BoardDAO;
+import model.BoardDTO;
+
 @Controller
 @RequestMapping({"/hello/*", "/hello"})
 public class HelloController {
-
+	@Autowired
+	private BoardDAO dao;
 	//예제
 	@GetMapping("")
 	public String hello(Model model,
@@ -19,27 +24,29 @@ public class HelloController {
 		return "contents";
 	}
 	
-	@PostMapping("board")
-	public String boardInsert() {
+	@PostMapping(value = "/board-insert")
+	public String boardInsert(BoardDTO dto,Model model) throws Exception {
+		dao.insert(dto);
 		//create  DB 연동 후 뷰페이지로 넘기기
 		return "?????";
 	}
 	
-	@GetMapping("board")
-	public String boardRead() {
+	@GetMapping(value = "/board-read")
+	public String boardRead(Model model,@RequestParam("boardId")int boardId) throws Exception {
+		dao.read(boardId);
 		//read  DB 연동 후 뷰페이지로 넘기기
 		return "?????";
 	}
 	
-	@GetMapping("board-update")
-	public String boardUpdate() {
-		//update  DB 연동 후 뷰페이지로 넘기기
+	@GetMapping(value = "/board-update")
+	public String boardUpdate(BoardDTO dto, Model model) throws Exception {
+		dao.update(dto);
 		return "?????";
 	}
 	
-	@GetMapping("board-delete")
-	public String boarDelete() {
-		//delete  DB 연동 후 뷰페이지로 넘기기
+	@GetMapping(value = "/board-delete")
+	public String boarDelete(@RequestParam("boardId")int boardId, Model model) throws Exception {
+		dao.delete(boardId);
 		return "?????";
 	}
 }
